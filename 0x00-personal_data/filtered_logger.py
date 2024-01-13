6fcbf1d16ff8597ccd7956b371c684342fc2d899
Imukua <ianmukuaa@gmail.com>
2024-01-13 13:57:43 +0300

    fixed regex again ☹️

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,20 +12,6 @@ def filter_datum(fields: List[str], redaction: str,
         Filters the data and reducts certain words
         """
         for val in fields:
-             message = re.sub(f"{val}=*?{separator}",
+             message = re.sub(f"{val}=.*?{separator}",
                               f"{val}={redaction}{separator}", message)
         return message
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
