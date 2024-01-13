5f64df3e49bcdc569edf9507585480f5c08a7166
Imukua <ianmukuaa@gmail.com>
2024-01-13 13:47:55 +0300

    added filter datum func

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,24 @@
+import logging
+from typing import List
+import re
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+        for val in fields:
+             message = re.sub(f"{val}=*?{separator}",
+                              f"{redaction}{separator}", message)
+        return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
