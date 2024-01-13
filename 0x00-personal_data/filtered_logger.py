08492c35024a1970abd62164c2b1ab101d59b331
Imukua <ianmukuaa@gmail.com>
2024-01-13 13:50:44 +0300

    made executable and shebanginized

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python3
+
+
 import logging
 from typing import List
 import re
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
+        """
+        Filters the data and reducts certain words
+        """
         for val in fields:
              message = re.sub(f"{val}=*?{separator}",
                               f"{redaction}{separator}", message)
