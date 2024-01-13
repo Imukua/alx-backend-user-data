5d98cf5b1ffa3764271848025115c21e2f5df0da
Imukua <ianmukuaa@gmail.com>
2024-01-13 14:30:03 +0300

    pycodestyle fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,15 +3,18 @@
 
 
 import logging
-from typing import List
 import re
+from typing import List
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-        """
-        Filters the data and reducts certain words
-        """
-        for val in fields:
-             message = re.sub(f"{val}=.*?{separator}",
-                              f"{val}={redaction}{separator}", message)
-        return message
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """
+    Filters the data and reducts certain words
+    """
+    for val in fields:
+        message = re.sub(
+            f"{val}=.*?{separator}", f"{val}={redaction}{separator}", message
+        )
+    return message
